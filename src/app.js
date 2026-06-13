cb9c5324f90a5fbba7b996d83e9c4d733b590264
Abdul-Ahad-503-Atsol <abdulahad.mushtaq@atsol.io>
2026-06-13 20:03:51 +0500

    hi.

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ app.use('/api/user', userRoutes);
 app.get('/', (req, res) => {
   res.json({
     success: true,
-    message: 'Welcome to Atendify API,sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd',
+    message: 'Welcome to Atendify API, working on version 2.0.0',
     version: '2.0.0',
     endpoints: {
       auth: '/api/auth',
